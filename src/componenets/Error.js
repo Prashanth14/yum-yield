82bfddc1d143d2c2ae2879b663190526309b50ab
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-01-01 16:20:07 -0500

    modified error page

--- src/componenets/Error.js
+++ src/componenets/Error.js
@@ -1,8 +1,13 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+    const err = useRouteError();
     return (
         <div>
             <h1>Oops!</h1>
             <h2>Something went wrong!!</h2>
+            <h3>{err.status} : {err.statusText}</h3>
+            <h3>{err.data}</h3>
         </div>
     )
 };
