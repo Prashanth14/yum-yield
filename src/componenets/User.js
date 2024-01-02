423a9e9e31e5d753b8a0c01bf21cbaf36af86651
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-01-02 17:27:44 -0500

    state variables in class based component

--- src/componenets/User.js
+++ src/componenets/User.js
@@ -1,6 +1,13 @@
+import { useState
+ } from "react";
 const User = (props) => {
+    const [count] = useState(0);
+    const [count2] = useState(1);
+
     return (
         <div className="user-card">
+            <h1>Count: {count}</h1>
+            <h1>Count2: {count2}</h1>
             <h2>Name: {props.name}</h2>
             <h3>Location: Atlanta</h3>
             <h4>Contact: @Prashanth14</h4>
--- src/componenets/UserClass.js
+++ src/componenets/UserClass.js
@@ -4,13 +4,21 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         console.log(props);
+
+        this.state = {
+            count: 0,
+            count2: 2,
+        };
     }
 
     render(){
         const {name, location} = this.props;
-        
+        const {count, count2} = this.state;
+
         return(
             <div className="user-card">
+                <h1>Count: {count}</h1>
+                <h1>Count2: {count2}</h1>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: @Prashanth14</h4>
