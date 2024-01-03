0ded822b4d142c210e661424fe4699d7a3f6aa22
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-01-02 19:31:37 -0500

    fetching Api call in class based componenet

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -1,15 +1,35 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () => {
-    return (
-    <div>
-        <h1>About Page</h1>
-        <h2>This is About Page, this is Prashanth Reddy Kadire</h2>
-        <User name={"Prashanth Function Component"}/>
-        <UserClass name={"Prashanth Class Based Component"} location={"Atlanta"}/>
-    </div>
-    );
-};
+class About extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>About Page</h1>
+                <h2>This is About Page, this is Prashanth Reddy Kadire</h2>
+                <UserClass name={"First"} location={"Atlanta"}/>
+            </div>
+            );
+    }
+}
+
+// const About = () => {
+//     return (
+//     <div>
+//         <h1>About Page</h1>
+//         <h2>This is About Page, this is Prashanth Reddy Kadire</h2>
+//         {/* <User name={"Prashanth Function Component"}/> */}
+//         <UserClass name={"Prashanth Class Based Component"} location={"Atlanta"}/>
+//     </div>
+//     );
+// };
 
 export default About;
--- src/componenets/UserClass.js
+++ src/componenets/UserClass.js
@@ -1,24 +1,35 @@
 import React from "react";
 
 class UserClass extends React.Component{
+
     constructor(props){
         super(props);
-        console.log(props);
 
         this.state = {
-            count: 0,
-            count2: 2,
-        };
+            userInfo:{
+                name:"Dummy",
+                location: "Default",
+                avatar_url: "http://dummy.jpg",
+            },
+        }
+    }
+
+    async componentDidMount(){
+        const data = await fetch("https://api.github.com/users/Prashanth14");
+        const json = await data.json();
+        console.log(json);
+
+        this.setState({
+            userInfo: json,
+        });
     }
 
     render(){
-        const {name, location} = this.props;
-        const {count, count2} = this.state;
-
+        const {name, location, avatar_url
+        } = this.state.userInfo;
         return(
             <div className="user-card">
-                <h1>Count: {count}</h1>
-                <h1>Count2: {count2}</h1>
+                <img src={avatar_url}/>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: @Prashanth14</h4>
