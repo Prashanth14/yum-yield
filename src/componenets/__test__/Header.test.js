0ccf7e4788888578998674cb13ddaf8f9b343647
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-01-07 16:56:21 -0500

    written test cases for login button and Cart items

---
+++ src/componenets/__test__/Header.test.js
@@ -0,0 +1,35 @@
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import Header from "../Header";
+import { render, screen } from "@testing-library/react";
+import appStore from "../../../utils/appStore"
+import "@testing-library/jest-dom";
+
+
+it("Should render Head Component with a login button", () => {
+    render(
+        <BrowserRouter>
+            <Provider store={appStore}>
+                <Header />
+            </Provider>
+        </BrowserRouter>
+    );
+
+    const loginButton = screen.getByRole("button", {name: "Login"});
+
+    expect(loginButton).toBeInTheDocument();
+});
+
+it("Should render Head Component with Cart 0 Items", () => {
+    render(
+        <BrowserRouter>
+            <Provider store={appStore}>
+                <Header />
+            </Provider>
+        </BrowserRouter>
+    );
+
+    const cartItems = screen.getByText("🛒 -(0 items)");
+
+    expect(cartItems).toBeInTheDocument();
+});
--- src/componenets/__test__/contact.test.js
+++ src/componenets/__test__/contact.test.js
@@ -2,35 +2,40 @@ import Contact from "../Contact";
 import {render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test("should load contact Us component", () => {
-    render(<Contact />);
-
-    const heading = screen.getByRole("heading");
-    //Assertion
-    expect(heading).toBeInTheDocument();
+//Unit Testing
+describe("Contact Us Page Test case", () => {
+    //you can use function name as either test or it
+    test("should load contact Us component", () => {
+        render(<Contact />);
+    
+        const heading = screen.getByRole("heading");
+        //Assertion
+        expect(heading).toBeInTheDocument();
+    });
+    //it is alias of test
+    it("should load button in contact Us component", () => {
+        render(<Contact />);
+    
+        const button = screen.getByRole("button");
+        //Assertion
+        expect(button).toBeInTheDocument();
+    });
+    
+    test("should load Input name in contact Us component", () => {
+        render(<Contact />);
+    
+        const button = screen.getByPlaceholderText("name");
+        //Assertion
+        expect(button).toBeInTheDocument();
+    });
+    
+    test("should load 2 Input names in contact Us component", () => {
+        render(<Contact />);
+        //Querying
+        const inputBoxes = screen.getAllByRole("textbox");
+    
+        //Assertion
+        expect(inputBoxes.length).toBe(2);
+    });
 });
 
-test("should load button in contact Us component", () => {
-    render(<Contact />);
-
-    const button = screen.getByRole("button");
-    //Assertion
-    expect(button).toBeInTheDocument();
-});
-
-test("should load Input name in contact Us component", () => {
-    render(<Contact />);
-
-    const button = screen.getByPlaceholderText("name");
-    //Assertion
-    expect(button).toBeInTheDocument();
-});
-
-test("should load 2 Input names in contact Us component", () => {
-    render(<Contact />);
-
-    const inputBoxes = screen.getAllByRole("textbox");
-
-    //Assertion
-    expect(inputBoxes.length).toBe(2);
-});
