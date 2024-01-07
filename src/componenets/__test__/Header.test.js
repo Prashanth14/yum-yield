af0d38ffa6df9a4a32e44e08ebd3e28400ccaad3
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-01-07 17:01:06 -0500

    written test cases for login button

--- src/componenets/__test__/Header.test.js
+++ src/componenets/__test__/Header.test.js
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../Header";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import appStore from "../../../utils/appStore"
 import "@testing-library/jest-dom";
 
@@ -32,4 +32,22 @@ it("Should render Head Component with Cart 0 Items", () => {
     const cartItems = screen.getByText("🛒 -(0 items)");
 
     expect(cartItems).toBeInTheDocument();
+});
+
+it("Should change Login button to Logout", () => {
+    render(
+        <BrowserRouter>
+            <Provider store={appStore}>
+                <Header />
+            </Provider>
+        </BrowserRouter>
+    );
+
+    const loginButton = screen.getByRole("button", {name: "Login"});
+
+    fireEvent.click(loginButton);
+
+    const logoutButton = screen.getByRole("button", {name: "Logout"});
+
+    expect(logoutButton).toBeInTheDocument();
 });
